b1039a2d836bc386732d78f99c547998bf66af4d
Arctic344 <arctic344.spam@gmail.com>
2023-10-24 11:38:19 +1030

    Made the library

--- library/Arctic344_PCA9685_motorDriver.cpp
+++ library/Arctic344_PCA9685_motorDriver.cpp
@@ -1,7 +1,26 @@
 #include "Arduino.h"
 #include "Arctic344_PCA9685_motorDriver.h"
 
-Arctic344_PCA9685_motorDriver::driver(uint8_t I2Caddr)
+Arctic344_PCA9685_motorDriver::Arctic344_PCA9685_motorDriver(uint8_t I2Caddr)
 {
   _addr = I2Caddr;
+  uint8_t[4][3] = {
+    {1,2,3}
+    {1,2,3}
+    {1,2,3}
+    {1,2,3}
+  } // motor connection points. motor number 1-4 with the three connection pints, In1, In2, PWM, in that order
+}
+
+
+void Arctic344_PCA9685_motorDriver::begin() {
+  Serial.println("began the driver")
+}
+
+void Arctic344_PCA9685_motorDriver::setMotorSpeed(uint8_t motorNumber, short motorSpeed) {
+  Serial.println("Setting a motors speed");
+  Serial.print("Motor:");
+  Serial.println(motorNumber);
+  Serial.print("Speed:");
+  Serial.println(motorSpeed);
 }
--- library/Arctic344_PCA9685_motorDriver.h
+++ library/Arctic344_PCA9685_motorDriver.h
@@ -2,10 +2,17 @@
 #define Arctic344_PCA9685_motorDriver
 
 #include "Arduino.h"
+#include <Adafruit_PWMServoDriver.h>
 
 class Arctic344_PCA9685_motorDriver
 {
-    
+public:
+    Arctic344_PCA9685_motorDriver();
+    void begin();
+    void setMotorSpeed(uint8_t motorNumber, short motorSpeed);
+private:
+    Adafruit_PWMServoDriver pwm;
+    uint8_t _addr;
 }
 
 #endif
